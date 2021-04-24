623cef2e177e4eefb225c5418cf1d0b63cb4a491
laxiflora <liu200113@gmail.com>
2021-04-25 02:42:20 +0800

    5.2Binary_tree實作

--- trees/binary_tree_5.2.cpp
+++ trees/binary_tree_5.2.cpp
@@ -19,7 +19,7 @@ class Binary_Tree{
         bool equal(Binary_Tree);   //is the tree equal
         int count(void);           //count nodes number
         int height(void);          //count tree height
-        void swap_sib(void);       //swap tree sibling
+        void swap_sib(Node*);       //swap tree sibling
         //expression
         //sorting
 
@@ -29,6 +29,8 @@ class Binary_Tree{
         Node* _copy(Node*);
         bool _equal(Node*,Node*);
         int _count(Node*);
+        int _height(Node*,int);
+
 };
 
 Binary_Tree::Binary_Tree(void){
@@ -96,13 +98,57 @@ int Binary_Tree::_count(Node* current){
 
 }
 
+int Binary_Tree::height(void){
+    if(isEmpty()) return 0;
+    else{
+        return _height(this->root,1);
+    }
+}
+
+int Binary_Tree::_height(Node* current,int height){
+    int rHeight=height;
+    int lHeight=height;
+    if(current->rChild!=NULL){
+        rHeight =  _height(current->rChild,height+1);
+    }
+    else if(current->lChild!=NULL){
+        lHeight = _height(current->lChild,height+1);
+    }
+    return max(rHeight,lHeight);
+}
+
+void Binary_Tree::swap_sib(Node* current){
+    if(current!=NULL){
+        swap_sib(current->lChild);
+        swap_sib(current->rChild);
+        Node* temp = current->lChild;
+        current->lChild = current->rChild;
+        current->rChild = temp;
+    }
+}
 
 int main(void){
     Binary_Tree alpha;
     alpha.root = new Node;
-    alpha.root->data = 87;
+    alpha.root->data = 32;
+    alpha.root->rChild = new Node;
+    alpha.root->rChild->lChild = new Node;
+    alpha.root->rChild->rChild = new Node;
+    alpha.root->rChild->lChild->data = 120;
+    alpha.root->rChild->rChild->data = 321;
+    alpha.root->lChild = new Node;
     Binary_Tree beta = alpha.copy();
-    cout<<beta.root->data<<endl;
-    cout<<alpha.equal(beta)<<endl;
-    cout<<alpha.count()<<endl;
+    cout<<"Copy--beta.root: "<<beta.root->data<<endl;
+    cout<<"is Equal: "<<alpha.equal(beta)<<endl;
+    cout<<"Node count: "<<alpha.count()<<endl;
+    cout<<"Height: "<<alpha.height()<<endl;
+    cout<<"Before Change: "<<alpha.root->rChild->lChild->data<<endl;
+    alpha.swap_sib(alpha.root->rChild);
+    cout<<"Changed: "<<alpha.root->rChild->lChild->data<<endl;
 }
+
+/*TODO
+swap sibling
+expression
+sorting
+*/
